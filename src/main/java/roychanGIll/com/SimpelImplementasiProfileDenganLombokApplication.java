faae73be9ac3276e5e39b46a7677fdc5ea43fef3
GilbrantRoychan <gilbrant.roychan123@gmail.com>
2026-03-04 11:49:28 +0700

    first commit

---
+++ src/main/java/roychanGIll/com/SimpelImplementasiProfileDenganLombokApplication.java
@@ -0,0 +1,13 @@
+package roychanGIll.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpelImplementasiProfileDenganLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpelImplementasiProfileDenganLombokApplication.class, args);
+	}
+
+}
